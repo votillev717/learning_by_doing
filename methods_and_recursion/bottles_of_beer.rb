9f7d05443a73f929ee8d32668e157011891a0f0e
Evan Votilla <votillev717@gmail.com>
2016-10-12 00:38:58 -0600

    131780323_methods_and_recursion_master

---
+++ methods_and_recursion/bottles_of_beer.rb
@@ -0,0 +1,21 @@
+#!usr/bin/env ruby
+
+def bottles_of_beer
+
+end
+
+number_of_bottles = 100
+number_of_bottles2 = 99
+
+until number_of_bottles < 2
+
+  number_of_bottles -= 1
+  number_of_bottles2 -= 1
+
+  puts "#{number_of_bottles} bottles of beer on the wall. "
+  puts "#{number_of_bottles} bottles of beer. Take one down pass it around. "
+  puts "#{number_of_bottles2} bottles of beer on the wall.
+
+"
+
+end
