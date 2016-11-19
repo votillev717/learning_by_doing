0a651f56b6eec6fd9470f9173e7cadf9397ecd6a
Evan Votilla <votillev717@gmail.com>
2016-11-19 11:15:53 -0700

    Made changes to punctuation.
    [#131780323]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -9,8 +9,8 @@ def bottles_of_beer
     number_of_bottles -= 1
     new_number_of_bottles -= 1
 
-    puts "#{number_of_bottles} bottles of beer on the wall. "
-    puts "#{number_of_bottles} bottles of beer. Take one down pass it around. "
+    puts "#{number_of_bottles} bottles of beer on the wall, "
+    puts "#{number_of_bottles} bottles of beer. Take one down pass it around, "
     puts "#{new_number_of_bottles} bottles of beer on the wall."
 
   end
