971640857122cad09db1da13f10b755d29b6b26e
Evan Votilla <votillev717@gmail.com>
2016-11-20 14:00:58 -0700

    Completed all Travis complaints.
    [#131780323]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -9,11 +9,34 @@ def bottles_of_beer
     number_of_bottles -= 1
     new_number_of_bottles -= 1
 
-    puts "#{number_of_bottles} bottles of beer on the wall, "
-    puts "#{number_of_bottles} bottles of beer. Take one down pass it around, "
+    print "#{number_of_bottles} bottles of beer on the wall, "
+    puts "#{number_of_bottles} bottles of beer. "
+    print "Take one down pass it around, "
     puts "#{new_number_of_bottles} bottles of beer on the wall."
 
   end
+
+  if number_of_bottles == 2
+    print "#{number_of_bottles} bottle of beer on the wall, "
+    puts "#{number_of_bottles} bottles of beer. "
+    print "Take one down pass it around, "
+    puts "#{new_number_of_bottles} bottle of beer on the wall. "
+  end
+
+  if number_of_bottles == 1
+    print "#{number_of_bottles} bottle of beer on the wall, "
+    puts "#{number_of_bottles} bottle of beer. "
+    print "Take one down pass it around, "
+    puts "no more bottles of beer on the wall. "
+  end
+
+  print "No more bottles of beer on the wall, "
+  puts "no more bottles of beer. "
+  print "Go to the store and buy some more, "
+  puts "99 bottles of beer on the wall. "
+
+
 end
+
 
 puts bottles_of_beer
