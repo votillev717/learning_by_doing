41fff0ca52a422dff74f59d3f8b5a03402773e23
Evan Votilla <votillev717@gmail.com>
2016-11-20 18:04:11 -0700

    Made some updates to pass the tests.
    [#131780323]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,42 +1,40 @@
 #!/usr/bin/env ruby
 
 def bottles_of_beer
-  number_of_bottles = 100
-  new_number_of_bottles = 99
+  number_of_bottles = 99
 
-  until number_of_bottles < 2
-
-    number_of_bottles -= 1
-    new_number_of_bottles -= 1
+  until number_of_bottles == 2
+    new_bottles = number_of_bottles
 
     print "#{number_of_bottles} bottles of beer on the wall, "
     puts "#{number_of_bottles} bottles of beer. "
-    print "Take one down pass it around, "
-    puts "#{new_number_of_bottles} bottles of beer on the wall."
+    print "Take one down and pass it around, "
+    puts "#{new_bottles - 1} bottles of beer on the wall."
+    puts ''
+
+    number_of_bottles -= 1
 
   end
 
-  if number_of_bottles == 2
-    print "#{number_of_bottles} bottle of beer on the wall, "
-    puts "#{number_of_bottles} bottles of beer. "
-    print "Take one down pass it around, "
-    puts "#{new_number_of_bottles} bottle of beer on the wall. "
-  end
+  print "#{number_of_bottles} bottles of beer on the wall, "
+  puts "#{number_of_bottles} bottles of beer. "
+  print "Take one down and pass it around, "
+  puts "#{number_of_bottles - 1} bottle of beer on the wall. "
+  puts ''
 
-  if number_of_bottles == 1
-    print "#{number_of_bottles} bottle of beer on the wall, "
-    puts "#{number_of_bottles} bottle of beer. "
-    print "Take one down pass it around, "
-    puts "no more bottles of beer on the wall. "
-  end
+  number_of_bottles -= 1
+
+  print "#{number_of_bottles} bottle of beer on the wall, "
+  puts "#{number_of_bottles} bottle of beer. "
+  print "Take one down and pass it around, "
+  puts "no more bottles of beer on the wall. "
+  puts ''
 
   print "No more bottles of beer on the wall, "
   puts "no more bottles of beer. "
   print "Go to the store and buy some more, "
   puts "99 bottles of beer on the wall. "
 
-
 end
 
-
-puts bottles_of_beer
+bottles_of_beer
